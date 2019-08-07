0e515d9c7f3333c003701d45d3e3b6c2debae0e3
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-07 12:14:53 +0200

    models added to the exo

--- db/migrate/20190807094814_create_restaurants.rb
+++ db/migrate/20190807094814_create_restaurants.rb
@@ -5,7 +5,7 @@ class CreateRestaurants < ActiveRecord::Migration[5.2]
       t.string :address
       t.integer :phone_number
       t.string :category
-      t.text :reviews
+      t.integer :reviews
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_094814) do
+ActiveRecord::Schema.define(version: 2019_08_07_100519) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.integer "phone_number"
     t.string "category"
-    t.text "reviews"
+    t.integer "reviews"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
