99190116754b125c05db63a02387c0dc89efb3d4
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-07 12:38:39 +0200

    raking all the model specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_100519) do
+ActiveRecord::Schema.define(version: 2019_08_07_103136) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "phone_number"
+    t.string "phone_number"
     t.string "category"
     t.integer "reviews"
     t.datetime "created_at", null: false
